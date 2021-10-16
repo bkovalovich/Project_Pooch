17b9f0f0b14dae2f96fb8cc15669b3ff644bd712
dampNoodle <68832253+dampNoodle@users.noreply.github.com>
2021-10-15 17:40:13 -0700

    Portals & Levels
    Added portals to next levels and levels, so the gameplay actually loops correctly

--- mobile_proto/Assets/Scripts/GameManagerScript.cs
+++ mobile_proto/Assets/Scripts/GameManagerScript.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //Manages level progression within the main game scene
 public class GameManagerScript : MonoBehaviour
 {
-    private int level;
     public GameObject enemyPrefab; //Basic enemy prefab
+    public GameObject portalPrefab;
     private int destroyedEnemies = 0; //Number of destoryed enemies
+
+    public static int level = 1;//Current Level
 
     [SerializeField] public int range; //Size of possible spawns 
     [SerializeField] public GameObject background; //In order to maintain spawn on the map
+    [SerializeField] public Text levelText;
     [SerializeField] public int amountOfEnemies; //Number of enemies per round
 
     //Start()
     //Creates enemies at random positions
     public void Start()
     {
-        destroyedEnemies = 0;
+        levelText.text = levelText.text + level;
         for(int i = 0; i < amountOfEnemies; i++) {
             Instantiate(enemyPrefab, RandomMapPosition(), new Quaternion(0, 0, 0, 0));
         }
@@ -39,8 +43,11 @@ public class GameManagerScript : MonoBehaviour
     //FixedUpdate()
     //Starts next round if all enemies were destroyed
     public void FixedUpdate() {
+        //Debug.Log(level);
         if (destroyedEnemies == amountOfEnemies) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Instantiate(portalPrefab, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
+            level++;
+            destroyedEnemies++;
         }
     }
 }
---
+++ mobile_proto/Assets/Scripts/NextLevelPortalScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelPortalScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
